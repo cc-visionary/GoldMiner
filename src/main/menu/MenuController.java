c7b1bdbe118188e28ceec295ea26a46d67df57c1
Christopher Lim <lim.christopher.go@gmail.com>
2020-11-21 13:52:15 +0800

    Add Editable size of Board

--- src/main/menu/MenuController.java
+++ src/main/menu/MenuController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Spinner;
 import javafx.stage.Stage;
 import main.gameScreen.GameScreenController;
 import main.model.Board;
@@ -13,6 +14,9 @@ import main.model.Board;
 import java.io.IOException;
 
 public class MenuController {
+    @FXML
+    private Spinner spinner;
+
     @FXML
     public void chooseRandom(ActionEvent ae) throws IOException {
         goToGameScreen(ae, 'R');
@@ -34,7 +38,7 @@ public class MenuController {
 
     private FXMLLoader generateGameScreenLoader(char choice) {
         FXMLLoader gameScreenLoader = new FXMLLoader(getClass().getResource("/main/gameScreen/GameScreen.fxml"));
-        gameScreenLoader.setController(new GameScreenController(10, choice));
+        gameScreenLoader.setController(new GameScreenController((int) spinner.getValue(), choice));
 
         return gameScreenLoader;
     }
